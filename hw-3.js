256814e99a1db528a672a042f0099d849c3e2293
3213e <Linar.abv@mail.com>
2025-02-10 21:48:12 +0300

    Выполнил задание №1

---
+++ hw-3.js
@@ -0,0 +1,7 @@
+let password = 'Пароль';
+let userInput = prompt("Введите пароль:");
+if (userInput === password) {
+  alert ("Пароль введен верно");
+} else {
+  alert ("Пароль введен неправильно");
+}
